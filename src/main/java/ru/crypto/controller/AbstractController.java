469e1ea4d13f31a4ecf0dd3a66c28c802506c24a
nekitos911 <n.i.k.tver@yandex.ru>
2018-08-12 15:18:38 +0300

    Fix

--- src/main/java/ru/crypto/controller/AbstractController.java
+++ src/main/java/ru/crypto/controller/AbstractController.java
@@ -1,4 +1,8 @@
 package ru.crypto.controller;
 
 public abstract class AbstractController {
+
+    public abstract String getConverted(String crypto, String currency);
+
+    public abstract void saveData(String crypto, String currency, double cryptoValue, double currencyValue);
 }
